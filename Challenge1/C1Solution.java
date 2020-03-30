7f88b0766605982070784d44507e3b023c6177b1
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-03-30 00:54:15 -0400

    update algorithm

--- Challenge1/C1Solution.java
+++ Challenge1/C1Solution.java
@@ -9,22 +9,36 @@ public class C1Solution {
         
         int[] array = {1, 3, 6, 4, 1, 2};
 
-        int smallest;
-        int current;
-        int behindCurrent;
+        int smallest = 100000000;
+        
         
         for(int i = 0; i < array.length; i++){
-            current = array[i+1];
-            behindCurrent = array[i];
+            int current = 0;
+            int behindCurrent = 0;
+            if(i == array.length - 1){
+                current = array[i];
+            }
+            else{
+                current = array[i+1];
+                behindCurrent = array[i];
+            }
+            
 
-            if(behindCurrent >= 1 && current >= 1 && behindCurrent <= current-2){
+            if(behindCurrent >= 1 && current >= 1 && behindCurrent <= current-2 && behindCurrent + 1 < smallest){
                 smallest = behindCurrent + 1;
             }
-            else if(behindCurrent >= 1 && current >= 1 && current <= behindCurrent-2){
-                
+            else if(behindCurrent >= 1 && current >= 1 && current <= behindCurrent-2 && current + 1 < smallest){
+                smallest = current + 1;
+            }
+            else if(current == behindCurrent){
+                if(current + 1 < smallest){
+                    smallest = current + 1;
+                }
             }
 
         }
+
+        System.out.println(smallest);
 
     }
 }

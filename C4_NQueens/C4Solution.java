0519dd027e6fa987ae48dfeb843e5133b2907937
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-14 00:42:54 -0400

    valid place check

--- C4_NQueens/C4Solution.java
+++ C4_NQueens/C4Solution.java
@@ -40,5 +40,24 @@ public class C4Solution {
 		}
 		
 		return false;
+    }
+    
+    private boolean isPlaceValid(int rowIndex, int colIndex) {
+		
+		for(int i=0;i<colIndex;i++)
+			if( chessTable[rowIndex][i] == 1)
+				return false;
+		
+		for(int i=rowIndex, j=colIndex;i>=0 && j>=0;i--,j--) {
+			if( chessTable[i][j] == 1 ) 
+				return false;
+		}
+		
+		for(int i=rowIndex, j=colIndex;i<chessTable.length && j>=0;i++,j--) {
+			if( chessTable[i][j] == 1)
+				return false;
+		}
+		
+		return true;
 	}
 }

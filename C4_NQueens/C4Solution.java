98fd3b45cca7eff5484aff3cbedc0cbea54ea8a2
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-14 00:40:31 -0400

    setQueens method

--- C4_NQueens/C4Solution.java
+++ C4_NQueens/C4Solution.java
@@ -7,5 +7,29 @@ public class C4Solution {
 	public C4Solution(int numOfQueens) {
 		this.chessTable = new int[numOfQueens][numOfQueens];
 		this.numOfQueens = numOfQueens;
+    }
+    
+    private boolean setQueens(int colIndex) {
+
+		if( colIndex == numOfQueens ) {
+			return true;
+		}
+		
+		for(int rowIndex=0;rowIndex<numOfQueens;++rowIndex) {
+			
+			if(isPlaceValid(rowIndex, colIndex) ) {
+				
+				chessTable[rowIndex][colIndex] = 1;
+				
+				if( setQueens(colIndex+1) ) {
+					return true;
+				}
+				
+				// BACKTRACKING !!!
+				chessTable[rowIndex][colIndex] = 0;
+			}
+		}
+		
+		return false;
 	}
 }

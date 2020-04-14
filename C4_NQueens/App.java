97dbda22bcc188b038365b4f88392a4c7f5036d4
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-14 00:38:36 -0400

    added App file

---
+++ C4_NQueens/App.java
@@ -0,0 +1,5 @@
+package C4_NQueens;
+
+public class App {
+
+}
--- C4_NQueens/C4Solution.java
+++ C4_NQueens/C4Solution.java
@@ -1,5 +1,11 @@
 package C4_NQueens;
 
 public class C4Solution {
-
+    private int[][] chessTable;
+	private int numOfQueens;
+	
+	public C4Solution(int numOfQueens) {
+		this.chessTable = new int[numOfQueens][numOfQueens];
+		this.numOfQueens = numOfQueens;
+	}
 }

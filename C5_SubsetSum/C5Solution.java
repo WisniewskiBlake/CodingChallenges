1857cb24e3bb97837477dbea64ec2e286741c318
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-16 02:50:18 -0400

    contructor/variables

--- C5_SubsetSum/C5Solution.java
+++ C5_SubsetSum/C5Solution.java
@@ -1,5 +1,13 @@
 package C5_SubsetSum;
 
 public class C5Solution {
-
+    private boolean[][] dpTable;
+	private int[] numbers;
+	private int sum;
+	
+	public C5Solution(int[] numbers, int sum){
+		this.numbers = numbers;
+		this.sum = sum;
+		this.dpTable = new boolean[numbers.length + 1][sum + 1];	
+	}
 }

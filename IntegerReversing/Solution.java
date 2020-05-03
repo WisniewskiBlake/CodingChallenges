80bfcc0521051eaeff6172a17320b146adb6d214
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-03 03:30:18 -0400

    reversed w/ no conversion

--- IntegerReversing/Solution.java
+++ IntegerReversing/Solution.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println(reverse(1234));
+        System.out.println(reverseNoConversion(1234));
     }
 
     public static int reverse(int number) {
@@ -21,6 +21,19 @@ public class Solution {
             reversed += digit;
         }
         return Integer.parseInt(reversed);
+    }
+
+    public static int reverseNoConversion(int number) {
+
+        int reversed = 0;
+        int digit;
+
+        while(number > 0) {
+            digit = number % 10;
+            number = number / 10;
+            reversed = reversed * 10 + digit;
+        }
+        return reversed;
     }
 
 

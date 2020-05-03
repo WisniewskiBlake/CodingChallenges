91ef34f78b071a91f867fcf2b5314a458665c775
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-03 18:24:38 -0400

    taking n/2 worst case approach

--- TwoEggs/TwoEggs.java
+++ TwoEggs/TwoEggs.java
@@ -4,4 +4,11 @@ public class TwoEggs {
     public static void main(String[] args) {
 
     }
+
+    //solve with dynamic programming, where n is number of eggs and m is
+    // number of floors
+    public static void solve(int n, int m) {
+        int lastMid;
+        int mid = m/2;
+    }
 }

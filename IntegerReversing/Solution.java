112df3f608a947283bd7bdd6d317b668beaf7d8f
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-03 03:26:16 -0400

    first implementation

--- IntegerReversing/Solution.java
+++ IntegerReversing/Solution.java
@@ -1,11 +1,27 @@
 package IntegerReversing;
 
+import java.util.HashMap;
+
 public class Solution {
 
     public static void main(String[] args) {
-
+        System.out.println(reverse(1234));
     }
 
+    public static int reverse(int number) {
+
+        String reversed = "";
+
+        int digit;
+//        int newNumber;
+
+        while(number > 0) {
+            digit = number % 10;
+            number = number / 10;
+            reversed += digit;
+        }
+        return Integer.parseInt(reversed);
+    }
 
 
 }

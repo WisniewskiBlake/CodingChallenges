d588888b694661a97f9c4e07615e47ffa31c0080
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-04 19:19:03 -0400

    created anagram challenge

---
+++ Anagram/Anagram.java
@@ -0,0 +1,17 @@
+package Anagram;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        String word1 = "restful";
+        String word2 = "fluster";
+
+        System.out.println(solve(word1, word2));
+    }
+
+    public static String solve(String word1, String word2) {
+
+        return "Not an anagram";
+    }
+
+}

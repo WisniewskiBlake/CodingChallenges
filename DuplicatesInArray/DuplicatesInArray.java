930820c6b20e62ad92e292c57725dcb23515a0f6
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-04 04:02:15 -0400

    solved in O(n) time

--- DuplicatesInArray/DuplicatesInArray.java
+++ DuplicatesInArray/DuplicatesInArray.java
@@ -1,12 +1,24 @@
 package DuplicatesInArray;
 
+import java.util.HashMap;
+
 public class DuplicatesInArray {
 
     public static void main(String[] args) {
-
+        int[] array = {4,7,6,7,5,1,2,3,9,2};
+        solve(array);
     }
 
     public static void solve(int[] array){
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < array.length; i++) {
+            map.put(array[i], i);
+
+            if(map.containsKey(array[i])) {
+                System.out.println(array[i]);
+            }
+        }
 
     }
 

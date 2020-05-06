41a7ace8d7d5f9b04bb02ebeda2120fc852a3f57
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-05 20:40:45 -0400

    logic and vars

--- LargestSubarray/LargestSubarray.java
+++ LargestSubarray/LargestSubarray.java
@@ -8,7 +8,16 @@ public class LargestSubarray {
     }
 
     public static int solve(int[] nums) {
+        int max_global = nums[0];
+        int max_current = nums[0];
 
+        for(int i=1; i<nums.length; i++) {
+            max_current = Math.max(nums[i], max_current+nums[i]);
+
+            if(max_current>max_global) {
+                max_global = max_current;
+            }
+        }
     }
 
 }

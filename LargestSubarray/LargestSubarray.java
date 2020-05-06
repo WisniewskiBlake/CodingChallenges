af35fccbcc6f52139c246789b6bc49305520dde2
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-05 20:35:15 -0400

    driver code

--- LargestSubarray/LargestSubarray.java
+++ LargestSubarray/LargestSubarray.java
@@ -2,6 +2,8 @@ package LargestSubarray;
 
 public class LargestSubarray {
     public static void main(String[] args) {
+        int[] nums = {1, -2, 3, 4, -5, 8};
 
+        System.out.println(solve(nums));
     }
 }

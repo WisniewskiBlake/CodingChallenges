fa5e9d380870e0c9835035ec4b44c5f7c56463e5
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-07 22:45:06 -0400

    create memoize solutions

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -1,8 +1,14 @@
 package Fibonacci;
 
+import java.util.Map;
+
 public class Fibonacci {
 
-    public int bruteForceFibonacci(int n) {
+    public static void main(String[] args) {
+        System.out.println(bruteForceFibonacci(4));
+    }
+
+    public static int bruteForceFibonacci(int n) {
         if(n == 0){
             return 0;
         }
@@ -10,6 +16,10 @@ public class Fibonacci {
             return 1;
         }
         return bruteForceFibonacci(n-1) + bruteForceFibonacci(n-2);
+    }
+
+    public static int memoizeFibonacci() {
+        Map<Integer,Integer> memoizeTable;
     }
 
 }

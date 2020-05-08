5eb193ecfcd9be193c18759dead112aebf115a37
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-07 22:18:54 -0400

    naive fibonacci base case

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -1,4 +1,16 @@
 package Fibonacci;
 
 public class Fibonacci {
+
+    public int bruteForceFibonacci(int n) {
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+
+
+    }
+
 }

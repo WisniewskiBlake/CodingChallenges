b64a5840abbac53f03dc6e8da6769d6fc13e2b16
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-08 00:18:41 -0400

    testing time of memoize with n=10

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println(bruteForceFibonacci(4));
+        System.out.println(memoizeFibonacci(10));
     }
 
     public static int bruteForceFibonacci(int n) {

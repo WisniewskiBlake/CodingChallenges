de1b86f7dc1ba84c05a2aa6f3318cd85444269f1
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-08 20:35:12 -0400

    loop conditions

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -46,6 +46,11 @@ public class Fibonacci {
         }
 
         int[] memo = new int[n];
+        memo[0] = 0;
+        memo[1] = 1;
+        for(int i = 2; i < n; i++) {
+            memo[i] = memo[i - 1] + memo[i-2];
+        }
 
     }
 

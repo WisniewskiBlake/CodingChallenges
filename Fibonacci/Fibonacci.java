ed2f110b63f6daf838051105a396673d39da67be
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-08 20:31:31 -0400

    created base case

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -38,6 +38,14 @@ public class Fibonacci {
     }
 
     public static int withoutMapFibonacci(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        else if(n == 1) {
+            return 1;
+        }
+
+        int[] memo = new int[n];
 
     }
 

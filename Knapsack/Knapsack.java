33a2d5cec7960e764daac2940822c2367e397a90
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-09 03:37:23 -0400

    loop logic

--- Knapsack/Knapsack.java
+++ Knapsack/Knapsack.java
@@ -8,5 +8,11 @@ public class Knapsack {
     public static void solve(int numOfItems, int capacity, int[] weights,
                              int[] values) {
 
+        for(int i=1; i< numOfItems; i++) {
+            for(int w = 1; w<capacity+1; w++) {
+
+            }
+        }
+
     }
 }

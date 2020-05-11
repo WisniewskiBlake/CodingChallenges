cbcfa84f65c565d197c2c5ec6c2e6087953382b9
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-11 04:17:14 -0400

    created map solution

--- RomanToInt/RomanToInt.java
+++ RomanToInt/RomanToInt.java
@@ -1,65 +1,20 @@
 package RomanToInt;
 
+import java.util.HashMap;
+
 public class RomanToInt {
     public static void main(String[] args) {
         System.out.println(romanToInt("IV"));
     }
 
     public static int romanToInt(String s) {
+        //assign symbol hierarchy
+        //if a symbol appears AFTER a larger(or equal) symbol it is added
+        //if a symbol appears BEFORE a larger symbol it is subtracted
         int number = 0;
-        //cycle through string with .charAt(i) in reverse
-        for (int i = s.length() - 1; i > -1; i--) {
-            if(i == s.length()) {
-                if(s.charAt(i) == 'M') {
-                    number += 1000;
-                }
-                else if(s.charAt(i) == 'D') {
-                    number += 500;
-                }
-                else if(s.charAt(i) == 'C') {
-                    number += 100;
-                }
-                else if(s.charAt(i) == 'L') {
-                    number += 50;
-                }
-                else if(s.charAt(i) == 'X') {
-                    number += 10;
-                }
-                else if(s.charAt(i) == 'V') {
-                    number += 5;
-                }
-                else if(s.charAt(i) == 'I') {
-                    number += 1;
-                } else {
-                    return 0;
-                }
-            } else {
-                if(s.charAt(i) == 'M') {
-                    number += 1000;
-                }
-                else if(s.charAt(i) == 'D') {
-                    number += 500;
-                }
-                else if(s.charAt(i) == 'C') {
-                    number += 100;
-                }
-                else if(s.charAt(i) == 'L') {
-                    number += 50;
-                }
-                else if(s.charAt(i) == 'X') {
-                    number += 10;
-                }
-                else if(s.charAt(i) == 'V') {
-                    number += 5;
-                }
-                else if(s.charAt(i) == 'I') {
-                    number += 1;
-                } else {
-                    return 0;
-                }
-            }
+        HashMap<> map = new HashMap();
 
-        }
+
         return number;
     }
 }

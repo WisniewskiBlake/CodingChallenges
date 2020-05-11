a5b8f1d32c61ff57c33771610f9eed159ab02683
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-11 02:46:49 -0400

    naive approach

--- ReverseInt/ReverseInt.java
+++ ReverseInt/ReverseInt.java
@@ -2,11 +2,26 @@ package ReverseInt;
 
 public class ReverseInt {
     public static void main(String[] args) {
-        System.out.println(reverse(123));
+        System.out.println(reverseNaive(123));
     }
 
-    public static int reverse(int x) {
+    public static int reverseNaive(int x) {
+        //convert to string
+        String y = Integer.toString(x);
+        int p = y.length();
+        int result = 0;
+        
+        //for loop through y and use .charAt(i) to access element
+        for (int i = p; i > 0; i--) {
+            result += Integer.parseInt(String.valueOf(y.charAt(i)));
+        }
+        //create partition from middle
 
+        if(y.length() % 2 == 0) {
+
+        }else {
+            
+        }
 
         return x;
     }

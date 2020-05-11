dc9d105d63c2b9d8a450da19c28c46f39d6ccb4c
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-11 05:12:35 -0400

    fixed edge cases

--- RomanToInt/RomanToInt.java
+++ RomanToInt/RomanToInt.java
@@ -9,12 +9,14 @@ import java.util.List;
 
 public class RomanToInt {
     public static void main(String[] args) {
-        System.out.println(romanToInt("X"));
+        System.out.println(romanToInt("LVIII"));
     }
 
     public static int romanToInt(String s) {
         //assign symbol hierarchy
         //if a symbol appears AFTER a larger(or equal) symbol it is added
+        // (from left > right, if i+1 is greater or equal, then add it. else
+        // subtract)
         //if a symbol appears BEFORE a larger symbol it is subtracted
         int number = 0;
         HashMap<Character,Integer> map = new HashMap<Character,Integer>();
@@ -30,27 +32,17 @@ public class RomanToInt {
             return map.get(s.charAt(0));
         }
         //need to check if i-1 is higher, lower, or equal
-        for (int i = s.length()-1; i > -1 ; i--) {
-            if(i == 0) {
-                if(map.get(s.charAt(i)) > map.get(s.charAt(i+1))) {
-                    number += map.get(s.charAt(i));
-                }
-                else if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))) {
-                    number -= map.get(s.charAt(i));
-                }
-                else if(map.get(s.charAt(i)) == map.get(s.charAt(i+1))) {
-                    number += map.get(s.charAt(i));
-                }
-            }
-            else if(map.get(s.charAt(i)) > map.get(s.charAt(i-1))) {
+        for (int i = 1; i < s.length(); i++) {
+            if(i == s.length()-1) {
                 number += map.get(s.charAt(i));
             }
-            else if(map.get(s.charAt(i)) < map.get(s.charAt(i-1))) {
-                number -= map.get(s.charAt(i));
+            if(map.get(s.charAt(i-1)) >= map.get(s.charAt(i))) {
+                number += map.get(s.charAt(i-1));
             }
-            else if(map.get(s.charAt(i)) == map.get(s.charAt(i-1))) {
-                number += map.get(s.charAt(i));
+            else if(map.get(s.charAt(i-1)) < map.get(s.charAt(i))) {
+                number -= map.get(s.charAt(i-1));
             }
+
         }
 
         return number;

3f26b9cc2c441e1568bc42cd2302c62f9fc0b8c0
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-13 22:21:28 -0400

    function test

--- ReverseString/ReverseString.java
+++ ReverseString/ReverseString.java
@@ -2,10 +2,23 @@ package ReverseString;
 
 public class ReverseString {
     public static void main(String[] args) {
-        System.out.println(solve(new String[]{"h", "e", "l", "l", "o"}));
+        String[] input = {"h", "e", "l", "l", "o"};
+        String[] output = solve(input);
+        for (int i = 0; i < output.length; i++) {
+            System.out.println(output[i]);
+        }
+
     }
 
-    public static char[] solve(String[] charArray) {
+    public static String[] solve(String[] stringArray) {
+        int size = stringArray.length - 1;
+        for (int i = 0; i < stringArray.length/2; i++) {
 
+            String temp = stringArray[i];
+            stringArray[i] = stringArray[size];
+            stringArray[size] = temp;
+            size--;
+        }
+        return stringArray;
     }
 }

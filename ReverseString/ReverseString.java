70c88f41fe775e3141cdbcf8f9ca5349a16a7c7a
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-13 23:13:56 -0400

    O(n) 1ms solution

--- ReverseString/ReverseString.java
+++ ReverseString/ReverseString.java
@@ -20,4 +20,16 @@ public class ReverseString {
             System.out.println(stringArray[i]);
         }
     }
+
+    public static void helper(char[] s, int left, int right) {
+        if (left >= right) return;
+        char tmp = s[left];
+        s[left++] = s[right];
+        s[right--] = tmp;
+        helper(s, left, right);
+    }
+
+    public static void reverseString(char[] s) {
+        helper(s, 0, s.length - 1);
+    }
 }

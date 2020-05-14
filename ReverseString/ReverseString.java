5720f6db0c05d17228a50f82a785132d7d34172e
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-13 22:43:13 -0400

    solved naive

--- ReverseString/ReverseString.java
+++ ReverseString/ReverseString.java
@@ -2,23 +2,22 @@ package ReverseString;
 
 public class ReverseString {
     public static void main(String[] args) {
-        String[] input = {"h", "e", "l", "l", "o"};
-        String[] output = solve(input);
-        for (int i = 0; i < output.length; i++) {
-            System.out.println(output[i]);
-        }
+        char[] input = {'h', 'e', 'l', 'l', 'e', 'e', 'e', 'h'};
+        solve(input);
+
 
     }
 
-    public static String[] solve(String[] stringArray) {
+    public static void solve(char[] stringArray) {
         int size = stringArray.length - 1;
         for (int i = 0; i < stringArray.length/2; i++) {
-
-            String temp = stringArray[i];
+            char temp = stringArray[i];
             stringArray[i] = stringArray[size];
             stringArray[size] = temp;
             size--;
         }
-        return stringArray;
+        for (int i = 0; i < stringArray.length; i++) {
+            System.out.println(stringArray[i]);
+        }
     }
 }

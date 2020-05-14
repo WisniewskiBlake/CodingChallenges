496d302681e07c9b0725491226e7036b416af211
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-14 02:42:03 -0400

    solved

--- MergeLists/MergeLists.java
+++ MergeLists/MergeLists.java
@@ -12,5 +12,13 @@ public class MergeLists {
         else if (l2 == null) {
             return l1;
         }
+        else if(l1.val > l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
+        }
     }
 }

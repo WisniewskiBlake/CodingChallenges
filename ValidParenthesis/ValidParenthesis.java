56c55507a79f3633aed7d6d4e5a31ce8f3d4bebc
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-14 01:37:12 -0400

    O(n) 1ms solution

--- ValidParenthesis/ValidParenthesis.java
+++ ValidParenthesis/ValidParenthesis.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class ValidParenthesis {
     public static void main(String[] args) {
-        System.out.println(isValid("()()()()"));
+        System.out.println(isValid("]"));
     }
 
     public static boolean isValid(String string) {
@@ -13,41 +13,28 @@ public class ValidParenthesis {
         Stack<Character> stack = new Stack<>();
         char[] leftParens = {'(', '{', '['};
         char[] rightParens = {')', '}', ']'};
-        int leftCounter = 0;
-        int rightCounter = 0;
         for (int i = 0; i < string.length(); i++) {
             stack.push(string.charAt(i));
-//            if(string.charAt(i) == leftParens[0]) {
-//                leftCounter++;
-//            } else if(string.charAt(i) == leftParens[1]) {
-//                leftCounter++;
-//            } else if(string.charAt(i) == leftParens[2]) {
-//                leftCounter++;
-//            }
-
             if(string.charAt(i) == rightParens[0]) {
                 stack.pop();
-                if(stack.peek() != leftParens[0]) {
+                if(stack.isEmpty() || stack.peek() != leftParens[0]) {
                     return false;
                 }
                 stack.pop();
             } else if(string.charAt(i) == rightParens[1]) {
                 stack.pop();
-                if(stack.peek() != leftParens[1]) {
+                if(stack.isEmpty() || stack.peek() != leftParens[1]) {
                     return false;
                 }
                 stack.pop();
             } else if(string.charAt(i) == rightParens[2]) {
                 stack.pop();
-                if(stack.peek() != leftParens[2]) {
+                if(stack.isEmpty() || stack.peek() != leftParens[2]) {
                     return false;
                 }
                 stack.pop();
             }
-
-
         }
-
-        return true;
+        return stack.isEmpty();
     }
 }

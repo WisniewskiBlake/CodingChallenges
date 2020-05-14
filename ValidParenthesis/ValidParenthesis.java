0471dac586bfc82ba73c45bce9b521a1d6e12da1
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-14 01:05:01 -0400

    added right parenthesis check

--- ValidParenthesis/ValidParenthesis.java
+++ ValidParenthesis/ValidParenthesis.java
@@ -13,25 +13,39 @@ public class ValidParenthesis {
         int leftCounter = 0;
         int rightCounter = 0;
         for (int i = 0; i < string.length(); i++) {
-            if(string.charAt(i) == rightParens[0]) {
-                rightCounter++;
-            } else if(string.charAt(i) == rightParens[1]) {
-                rightCounter++;
-            } else if(string.charAt(i) == rightParens[2]) {
-                rightCounter++;
-            }
-
             if(string.charAt(i) == leftParens[0]) {
                 leftCounter++;
+
             } else if(string.charAt(i) == leftParens[1]) {
                 leftCounter++;
+
             } else if(string.charAt(i) == leftParens[2]) {
                 leftCounter++;
+
             }
+
+            if(string.charAt(i) == rightParens[0]) {
+                rightCounter++;
+                if(string.charAt(i-1) != leftParens[0]) {
+                    return false;
+                }
+            } else if(string.charAt(i) == rightParens[1]) {
+                rightCounter++;
+                if(string.charAt(i-1) != leftParens[1]) {
+                    return false;
+                }
+            } else if(string.charAt(i) == rightParens[2]) {
+                rightCounter++;
+                if(string.charAt(i-1) != leftParens[2]) {
+                    return false;
+                }
+            }
+
+
         }
         if(leftCounter != rightCounter) {
             return false;
         }
-        return false;
+        return true;
     }
 }

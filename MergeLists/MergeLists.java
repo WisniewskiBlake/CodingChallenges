09a97263c43da21a22adc0075443a42bc8e5c8a8
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-14 02:35:23 -0400

    base cases

--- MergeLists/MergeLists.java
+++ MergeLists/MergeLists.java
@@ -4,4 +4,13 @@ public class MergeLists {
     public static void main(String[] args) {
 
     }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+    }
 }

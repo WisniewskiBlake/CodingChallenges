9e2b487eaf6db1fe89cb2bfb80541b4fc7241647
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-16 15:59:36 -0400

    solve method O(n) time

--- MaxSubarray/MaxSubarray.java
+++ MaxSubarray/MaxSubarray.java
@@ -15,7 +15,17 @@ public class MaxSubarray {
         System.out.println(solve(nums));
     }
 
-    public static int solve(int[] array) {
+    public static int solve(int[] nums) {
+        int max_global = nums[0];
+        int max_current = nums[0];
 
+        for(int i=1; i<nums.length; i++) {
+            max_current = Math.max(nums[i], max_current+nums[i]);
+
+            if(max_current>max_global) {
+                max_global = max_current;
+            }
+        }
+        return max_global;
     }
 }

3f25f9f953d7152ec6420493db61fb9ae491a7d0
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-21 19:20:51 -0400

    implemented dfs solution in O(NxM) time

--- NumberOfIslands/NumberOfIslands.java
+++ NumberOfIslands/NumberOfIslands.java
@@ -24,10 +24,14 @@ package NumberOfIslands;
 public class NumberOfIslands {
 
     public static void main(String[] args) {
-        char[][] array = {{'1','0','1','1'},{'1','0','1','1'},{'0','1','1','0'},{'1','1',
-                                                                     '1','1'}};
-        char[][] array2 = {{'1','0','1'},{'1','1','0'},{'1','0','1'}};
-        System.out.println(numIslands(array2));
+        char[][] array = {{'1','1','0','0','0'},{'1','1','0','0','0'},{'0',
+                                                                       '0','1',
+                                                               '0','0'},{'0',
+                                                                         '0',
+                                                                     '0','1',
+                                                                         '1'}};
+        char[][] array2 = {{'0','1','0'},{'1','0','1'},{'0','1','0'}};
+        System.out.println(numIslands3(array2));
     }
 
     public static int numIslands(char[][] grid) {
@@ -52,10 +56,17 @@ public class NumberOfIslands {
                         islands++;
                         counter++;
                     }
+                    else if(j == 0 && grid[i][j+1] == '0' && grid[i-1][j] == '0') {
+                        islands++;
+                    }
                     else if(j == grid[0].length - 1 && grid[i][j-1] == '1') {
+                        islands++;
                         islands = islands - counter;
                     }
                     else if(j == grid[0].length - 1 && grid[i-1][j] == '1') {
+
+                    }
+                    else if(j == grid[0].length - 1 && grid[i-1][j] == '0') {
 
                     }
                     else if(j == grid[0].length - 1 && i == grid.length - 1 &&
@@ -79,6 +90,9 @@ public class NumberOfIslands {
                     }
                     else if(grid[i][j+1] == '0' && grid[i-1][j] == '0' && grid[i][j-1] == '1') {
                         islands = islands - counter;
+                    }
+                    else if(grid[i][j+1] == '0' && grid[i-1][j] == '0' && grid[i][j-1] == '0') {
+                        islands++;
                     }
                     else if(grid[i][j+1] == '1' && grid[i-1][j] == '1' && grid[i][j-1] == '1') {
                         islands = islands - counter;
@@ -147,6 +161,41 @@ public class NumberOfIslands {
 
         }
         return islands;
+    }
+
+    static void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public static int numIslands3(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
     }
 
 

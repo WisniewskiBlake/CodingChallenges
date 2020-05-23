5939514dc7d93b9c5121cf634dd836a336963565
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-23 02:15:08 -0400

    added rightSideViewMethod

--- BSTRightSideView/BSTRightSideView.java
+++ BSTRightSideView/BSTRightSideView.java
@@ -14,11 +14,38 @@ package BSTRightSideView;
 //        \     \
 //        5     4       <---
 
+import java.util.*;
+
 public class BSTRightSideView {
     public static void main(String[] args) {
-
+        System.out.println(rightSideView(1,2,3,null,5,null,4));
     }
 
+    public static List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return Collections.emptyList();
 
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        List<Integer> res = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode n = queue.peek();
+            res.add(n.val);
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                // queue: left ... right
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+            }
+        }
+
+        return res;
+    }
 
 }

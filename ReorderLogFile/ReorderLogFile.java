fb8aee106671788b8f461734be48cad42c65e9a3
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-24 16:10:44 -0400

    splitting logs

--- ReorderLogFile/ReorderLogFile.java
+++ ReorderLogFile/ReorderLogFile.java
@@ -33,6 +33,8 @@ public class ReorderLogFile {
 
     public String[] reorderLogFiles(String[] logs) {
         Arrays.sort(logs, (log1, log2) -> {
+            String[] split1 = log1.split(" ", 2);
+            String[] split2 = log2.split(" ", 2);
 
         });
         return logs;

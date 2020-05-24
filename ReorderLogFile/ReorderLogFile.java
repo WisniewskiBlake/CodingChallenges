6244fe30f92a8b290757fbd75ba712f21bff2184
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-24 16:08:09 -0400

    added constraints

--- ReorderLogFile/ReorderLogFile.java
+++ ReorderLogFile/ReorderLogFile.java
@@ -19,6 +19,10 @@ package ReorderLogFile;
 //        Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
 //        Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
 
+//0 <= logs.length <= 100
+//        3 <= logs[i].length <= 100
+//        logs[i] is guaranteed to have an identifier, and a word after the identifier
+
 
 public class ReorderLogFile {
     public static void main(String[] args) {

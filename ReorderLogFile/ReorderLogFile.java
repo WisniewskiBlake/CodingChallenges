a54e491a5eca9ebfa1a639ce1d546d753208c67d
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-24 16:11:17 -0400

    checking for digits

--- ReorderLogFile/ReorderLogFile.java
+++ ReorderLogFile/ReorderLogFile.java
@@ -35,6 +35,8 @@ public class ReorderLogFile {
         Arrays.sort(logs, (log1, log2) -> {
             String[] split1 = log1.split(" ", 2);
             String[] split2 = log2.split(" ", 2);
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
 
         });
         return logs;

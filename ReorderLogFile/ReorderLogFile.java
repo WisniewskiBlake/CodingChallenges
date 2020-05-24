834767d852bd0e2e57b883afa3e3f296f7f4de0b
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-24 16:12:24 -0400

    logic in if statement

--- ReorderLogFile/ReorderLogFile.java
+++ ReorderLogFile/ReorderLogFile.java
@@ -38,7 +38,9 @@ public class ReorderLogFile {
             boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
             boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
             if (!isDigit1 && !isDigit2) {
-
+                int cmp = split1[1].compareTo(split2[1]);
+                if (cmp != 0) return cmp;
+                return split1[0].compareTo(split2[0]);
             }
             return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
         });

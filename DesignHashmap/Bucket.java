70069f979992db7f23ad705a3cb087a89cb2848d
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-28 20:27:51 -0400

    created constructor

--- DesignHashmap/Bucket.java
+++ DesignHashmap/Bucket.java
@@ -1,8 +1,12 @@
 package DesignHashmap;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Bucket {
     private List<Pair<Integer, Integer>> bucket;
 
+    public Bucket() {
+        this.bucket = new LinkedList<Pair<Integer, Integer>>();
+    }
 }

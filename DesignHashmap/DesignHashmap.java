eca7a49a8068264962e73c1ac037aab87578f10e
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-05-29 03:36:23 -0400

    added constructor

--- DesignHashmap/DesignHashmap.java
+++ DesignHashmap/DesignHashmap.java
@@ -1,10 +1,18 @@
 package DesignHashmap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DesignHashmap {
     private int key_space;
     private List<Bucket> hash_table;
 
+    public DesignHashmap() {
+        this.key_space = 2069;
+        this.hash_table = new ArrayList<Bucket>();
+        for (int i = 0; i < this.key_space; ++i) {
+            this.hash_table.add(new Bucket());
+        }
+    }
 
 }

f9b18a06cb91952c38ea590c0fb6781ffbaee3fd
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-06-09 02:02:57 -0400

    new use case - not solved

--- LongestSubstringWithoutRepeatingChars/Solution.java
+++ LongestSubstringWithoutRepeatingChars/Solution.java
@@ -4,21 +4,37 @@ import java.util.HashMap;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring("dvdf"));
     }
 
     public static int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         int currentLength = 0;
         int totalLength = 0;
-
-        for (int i = 0; i < s.length(); map.put(s.charAt(i), i), i++) {
-            if (map.containsKey(s.charAt(i)) && totalLength < currentLength) {
-                totalLength = currentLength;
-                currentLength = 0;
-            }
-            currentLength++;
+        if(s.length() == 0) {
+            return 0;
         }
-        return totalLength;
+        else if(s.length() == 1) {
+            return 1;
+        }
+        else {
+            for (int i = 0; i < s.length(); map.put(s.charAt(i), i), i++) {
+                if (map.containsKey(s.charAt(i)) && totalLength <= currentLength) {
+                    totalLength = currentLength;
+                    currentLength = 0;
+                }
+                currentLength++;
+            }
+            if(totalLength == 0) {
+                return currentLength;
+            }
+            if(totalLength < currentLength) {
+                return currentLength;
+            }
+            else {
+                return totalLength;
+            }
+
+        }
     }
 }

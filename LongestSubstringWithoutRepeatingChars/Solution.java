3691901bebf1e65f8b8d0f3e09d6357c2e3a753f
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-06-09 01:42:48 -0400

    solved i think

--- LongestSubstringWithoutRepeatingChars/Solution.java
+++ LongestSubstringWithoutRepeatingChars/Solution.java
@@ -1,13 +1,24 @@
 package LongestSubstringWithoutRepeatingChars;
 
+import java.util.HashMap;
+
 public class Solution {
     public static void main(String[] args) {
         System.out.println(lengthOfLongestSubstring("pwwkew"));
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        int substringLength = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int currentLength = 0;
+        int totalLength = 0;
 
-        return substringLength;
+        for (int i = 0; i < s.length(); map.put(s.charAt(i), i), i++) {
+            if (map.containsKey(s.charAt(i)) && totalLength < currentLength) {
+                totalLength = currentLength;
+                currentLength = 0;
+            }
+            currentLength++;
+        }
+        return totalLength;
     }
 }

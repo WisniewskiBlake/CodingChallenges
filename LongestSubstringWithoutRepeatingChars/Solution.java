6204e62b20a41cbbe5afef5d6f88ab830d037684
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-06-09 01:30:51 -0400

    solve method

--- LongestSubstringWithoutRepeatingChars/Solution.java
+++ LongestSubstringWithoutRepeatingChars/Solution.java
@@ -4,4 +4,10 @@ public class Solution {
     public static void main(String[] args) {
 
     }
+
+    public int lengthOfLongestSubstring(String s) {
+        int substringLength = 0;
+
+        return substringLength;
+    }
 }
